199359a8e006202f6df7c4d671223a21a43aa48f
Diego Holzer <diego.holzer@bluewin.ch>
2021-02-09 19:10:23 +0100

    Create Compare-EsxHostBuild.ps1

---
+++ VMware/vSphere/Compare-EsxHostBuild.ps1
@@ -0,0 +1,68 @@
+function Compare-EsxHostDatastore {
+    [CmdletBinding()]
+    param (
+        [Parameter(ValueFromPipeline, Mandatory = $true, Position = 0)]
+        $PrimaryHost,
+        [Parameter(ValueFromPipeline, Mandatory = $true, Position = 1)]
+        $SecondaryHost,
+        [Parameter(ValueFromPipeline, Mandatory = $false, Position = 2)]
+        [switch]$list = $false
+    )
+
+    begin {
+        $returnValue = @()
+    }
+
+    process {
+        if ($PrimaryHost.GetType().Name -ne 'VMHostImpl') {
+            $PrimaryHost = Get-VMHost $PrimaryHost
+        }
+        
+        if ($SecondaryHost.GetType().Name -ne 'VMHostImpl') {
+            $SecondaryHost = Get-VMHost $SecondaryHost
+        }
+
+        foreach ($primaryHostElement in $PrimaryHost) {
+            $primaryHostElementDatastore = $primaryHostElement | Get-Datastore
+            foreach ($secondaryHostElement in $SecondaryHost) {
+                $secondaryHostElementDatastore = $secondaryHostElement | Get-Datastore
+
+                $compareDatastore = Compare-Object -ReferenceObject $primaryHostElementDatastore -DifferenceObject $secondaryHostElementDatastore -IncludeEqual
+
+                if ($compareDatastore) {
+                    foreach ($compareDatastoreElement in ($compareDatastore | Sort-Object SideIndicator)) {
+                        if ($compareDatastoreElement.SideIndicator -eq '==') {
+                            $compareResult = 'Equal'
+                        }
+                        elseif ($compareDatastoreElement.SideIndicator -eq '=>') {
+                            $compareResult = 'missing'
+                        }
+                        elseif ($compareDatastoreElement.SideIndicator -eq '<=') {
+                            $compareResult = 'to much'
+                        }
+                        $value = [PSCustomObject]@{
+                            PrimaryVMHost = $primaryHostElement.Name
+                            SecondaryVMHost = $secondaryHostElement.Name
+                            Datastore = $compareDatastoreElement.InputObject.Name
+                            Status = $compareResult
+                        }
+                        $returnValue += $value        
+                    }
+                }
+            }
+        }
+    }
+
+    end {
+        if ($list) {
+            return ($returnValue | Sort-Object PrimaryVMHost, SecondaryVMHost)
+        }
+        
+        if ('missing' -in $returnValue.Status -OR 'to much' -in $returnValue.Status) {
+            return $false
+        }
+        else {
+            return $true
+        }
+    }
+}
