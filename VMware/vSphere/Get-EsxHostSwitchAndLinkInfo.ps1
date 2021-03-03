f79ccfa3a1645d40a94bddfff5a0dc6d26c0d1de
Diego Holzer <diego.holzer@bluewin.ch>
2021-03-03 13:42:06 +0100

    Create Get-EsxHostSwitchAndLinkInfo.ps1

---
+++ VMware/vSphere/Get-EsxHostSwitchAndLinkInfo.ps1
@@ -0,0 +1,53 @@
+function Get-EsxHostSwitchAndLinkInfo {
+    [CmdletBinding()]
+    param (
+        [Parameter(ValueFromPipeline, Mandatory = $true, Position = 0)]
+        $PrimaryHost,
+        [Parameter(ValueFromPipeline, Mandatory = $false, Position = 1)]
+        $Switch = ''
+    )
+
+    begin {
+        $returnValue = @()
+    }
+
+    process {
+        if ($PrimaryHost.GetType().Name -ne 'VMHostImpl') {
+            $PrimaryHost = Get-VMHost $PrimaryHost
+        }
+        foreach ($primaryHostElement in $PrimaryHost) {
+            
+            if ($Switch.Length -ne 0) {
+                $switchObject = $primaryHostElement | Get-VirtualSwitch -Name $Switch
+            }
+            else {
+                $switchObject = $primaryHostElement | Get-VirtualSwitch
+            }
+            
+            foreach ($switchElement in $switchObject) {
+                $switchElementNics = $primaryHostElement | Get-VMHostNetworkAdapter -DistributedSwitch $switchElement -Physical | Sort-Object Name
+                foreach ($switchElementNic in $switchElementNics) {
+                    $maxSpeed = $switchElementNic.ExtensionData.ValidLinkSpecification.SpeedMb | Sort-Object -Descending | Select-Object -First 1
+                    $currentSpeed = $switchElementNic.ExtensionData.LinkSpeed.SpeedMb
+                    $currentDuplex = $switchElementNic.ExtensionData.LinkSpeed.Duplex
+                    $duplexAvailable = ($true -in ($switchElementNic.ExtensionData.ValidLinkSpecification | Where-Object {$_.SpeedMb -eq $currentSpeed}).Duplex)            
+
+                    $value = [PSCustomObject]@{
+                        PrimaryHost = $primaryHostElement.Name
+                        Switch = $SwitchElement.Name
+                        Link = $SwitchElementNic
+                        Speed = $currentSpeed
+                        MaxSpeed = $maxSpeed
+                        Duplex = $currentDuplex
+                        DuplexAvailable = $duplexAvailable
+                    }
+                    $returnValue += $value
+                }    
+            }
+        }
+    }
+
+    end {
+        return ($returnValue | Sort-Object PrimaryHost, Link)
+    }
+}
