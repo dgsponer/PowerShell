1b168ac5e63aeb84ec36bb2d6bbc900f8ef10f9b
Diego Holzer <diego.holzer@bluewin.ch>
2021-03-31 11:21:24 +0200

    Create Get-EsxHostNicStats.ps1

---
+++ VMware/vSphere/Get-EsxHostNicStats.ps1
@@ -0,0 +1,43 @@
+function Get-EsxHostNicStats {
+    [CmdletBinding()]
+    param (
+        [Parameter(ValueFromPipeline, Mandatory = $true, Position = 0)]
+        $PrimaryHost,
+        [Parameter(ValueFromPipeline, Mandatory = $true, Position = 1)]
+        $VmNic,
+        [Parameter(ValueFromPipeline, Mandatory = $false, Position = 2)]
+        [switch]$AsJson
+    )
+
+    begin {
+        $returnValue = @()
+    }
+
+    process {
+        if ($PrimaryHost.GetType().Name -ne 'VMHostImpl') {
+            $PrimaryHost = Get-VMHost $PrimaryHost
+        }
+
+        foreach ($primaryHostElement in $PrimaryHost) {
+            foreach ($vmNicElement in $VmNic) {
+                $esxCli = Get-EsxCli -V2 -VMHost $primaryHostElement
+                $arguments = $esxCli.network.nic.stats.get.createArgs()
+                $arguments.nicname = $vmNicElement
+
+                $esxCliResult = $esxCli.network.nic.stats.get.Invoke($arguments)
+                $esxCliResult | Add-Member -Name 'VMHost' -Value $primaryHostElement.Name -MemberType NoteProperty
+                $returnValue += $esxCliResult
+            }
+        }
+    }
+
+    end {
+        $returnValue = $returnValue | Select-Object VMHost, * -ErrorAction Ignore
+        if ($AsJson) {
+            return ($returnValue | ConvertTo-Json -Depth 50)
+        }
+        else {
+            return $returnValue
+        }
+    }
+}
